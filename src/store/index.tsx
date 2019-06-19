27c10d28d27deaba10c9dd85d84ffe78e0cc7d3d
Serenity <d.palbanese@gmail.com>
2019-06-19 23:02:06 +0200

    Added necessary types

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,3 +1,22 @@
 export interface ApplicationState {
+    allWaifus: Waifu[];
+    userWaifus: Map<string, Waifu[]>;
+    globalLeader: User[];
+    leaderboards: Map<string, User[]>;
+}
+
+export interface Waifu {
+    name: string;
+    imageUrl: string;
+    rarity: number;
+    id: string;
+}
+
+export interface User {
+    rank: number;
+    avatarUrl: string;
+    name: string;
+    discrim: string;
+    exp: number;
 }
 
