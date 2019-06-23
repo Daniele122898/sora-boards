fe8ea3974cadc7ebcf2d8e62607b25b05c3af845
Serenity <d.palbanese@gmail.com>
2019-06-23 22:06:28 +0200

    Added Rarity Ascending and descending filter

---
+++ src/components/Filter.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+
+interface Props {
+    sorters:  Sorter[];
+    data: any[];
+    searchFilter: (el: any, search: string) => any;
+    dataReturn: (data: any) => any;
+    searchTextPlaceHolder?: string;
+}
+
+interface State {
+    searchText: string;
+    selectOption: string;
+}
+
+export interface Sorter {
+    name: string;
+    comparer: (firstEl: any, secondEl: any) => any;
+}
+
+class Filter extends React.Component<Props, State> {
+
+    constructor(props: any) {
+        super(props);
+        
+        this.state = {
+            searchText: '',
+            selectOption: props.sorters[0].name
+        }
+    }
+
+    searchTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const search = event.target.value;
+        // change state
+        this.setState(()=> ({
+            searchText: search
+        }));
+        // change the data array
+        const newData = this.props.data.filter((el) => {
+            return this.props.searchFilter(el, search);
+        });
+        // return it to the parent in its changed state.
+        this.props.dataReturn(newData);
+    }
+
+    selectChangeEvent = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const selected = event.target.value;
+        this.setState(()=> ({
+            selectOption: selected
+        }));
+        // now update the list
+        for (let i=0; i<this.props.sorters.length; i++) {
+            const sort = this.props.sorters[i];
+                if (sort.name.toLowerCase() === selected) {
+                // do your comparisons and return new list
+                this.props.data.sort(sort.comparer);
+                this.props.dataReturn(this.props.data);
+                break;
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div className="input-group">
+                <div className="input-group__item">
+                    <input 
+                        type="text"
+                        placeholder={this.props.searchTextPlaceHolder}
+                        className="text-input"
+                        value={this.state.searchText}
+                        onChange={this.searchTextChange}
+                    />
+                </div>
+                <div className="input-group__item">
+                    <select
+                        className="select"
+                        value={this.state.selectOption}
+                        onChange={this.selectChangeEvent}
+                    >
+                        {this.props.sorters.map((sorter, index) => (
+                            <option
+                                key={index} 
+                                value={sorter.name.toLowerCase()}
+                            >
+                                {sorter.name}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Filter;
--- src/components/WaifuList.tsx
+++ src/components/WaifuList.tsx
@@ -2,13 +2,45 @@ import React, { ReactChild } from 'react';
 import SplitScreen from './SplitScreen';
 import { Waifu } from '../store/index';
 import Pager from './Pager';
+import Filter from './Filter';
 
 interface Props {
     waifus: Waifu[];
     waifuMapper: (waifu: Waifu) => ReactChild;
 }
 
-class WaifuList extends React.Component<Props> {
+interface State {
+    waifus: Waifu[];
+}
+
+class WaifuList extends React.Component<Props, State> {
+
+    constructor(props: any) {
+        super(props);
+        
+        this.state = {
+            waifus: props.waifus
+        }
+    }
+
+    componentDidUpdate(prevProps: Props) {
+        if (prevProps.waifus.length != this.props.waifus.length) {
+            this.setState(() => ({
+                waifus: this.props.waifus
+            }));
+        }
+    }
+    
+    searchFilter(el: Waifu, search: string) {
+        return (el.name.includes(search));
+    }
+
+    dataReturn = (waifus: Waifu[]) => {
+        this.setState(()=> ({
+            waifus
+        }));
+    }
+    
     render() {
         return (
             <SplitScreen
@@ -16,8 +48,27 @@ class WaifuList extends React.Component<Props> {
               >
                 <div id="waifu-split" className="split">
                     <h1>Waifus</h1>
+                    <Filter
+                        sorters={[
+                            { 
+                                name: "Rarity Descending", 
+                                comparer: (first: Waifu, second: Waifu) => {
+                                    return first.rarity >= second.rarity ? -1 : 1;
+                                }
+                            }, {
+                                name: "Rarity Ascending", 
+                                comparer: (first: Waifu, second: Waifu) => {
+                                    return first.rarity <= second.rarity ? -1 : 1;
+                                }
+                            }
+                        ]}
+                        data={this.props.waifus}
+                        searchFilter={this.searchFilter}
+                        dataReturn={this.dataReturn}
+                        searchTextPlaceHolder="Waifu name to search..."
+                    />
                     <Pager 
-                      data={this.props.waifus}
+                      data={this.state.waifus}
                       pageModulo={12}
                       mapper={this.props.waifuMapper}
                     />
