36eb0163a76dd0fcf91cf78058a7fe0c0a110be2
Serenity <d.palbanese@gmail.com>
2019-06-27 23:04:13 +0200

    setup leader reducer

--- src/reducers/leaderReducer.ts
+++ src/reducers/leaderReducer.ts
@@ -1,5 +1,7 @@
 import { Reducer } from "redux";
 import { LeaderState } from "../store";
+import { LeaderAction } from '../actions/leaderActions';
+import { GET_GUILD_LEADERBOARD } from '../constants/index';
 
 const LeaderDefaultState: LeaderState = {
     leaderboards: new Map(),
@@ -7,8 +9,14 @@ const LeaderDefaultState: LeaderState = {
     firstFetch: false
 }
 
-const reducer: Reducer<LeaderState> = (state: LeaderState = LeaderDefaultState, action: {}) => {
-  switch (action/*.type*/) {
+const reducer: Reducer<LeaderState, LeaderAction> = (state: LeaderState = LeaderDefaultState, action: LeaderAction) => {
+  switch (action.type) {
+    case GET_GUILD_LEADERBOARD: 
+      return {
+        globalLeader: state.globalLeader,
+        firstFetch: state.firstFetch,
+        leaderboards: state.leaderboards.set(action.guildId, action.leaderboard)
+      }
     default:
       return state;
   }
